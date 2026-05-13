a91b9cc961c13e77e693d1b3a2817909ac057f3b
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-05-13 17:28:06 +0100

    Add `ExpandUrlValidator`

---
+++ src/LinkVault.Web.Api/Features/UrlShortening/ExpandUrl/ExpandUrlValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LinkVault.Core.Entities;
+using LinkVault.Core.Services;
+
+namespace LinkVault.Web.Api.Features.UrlShortening.ExpandUrl;
+
+public sealed class ExpandUrlValidator : AbstractValidator<ExpandUrlRequest>
+{
+    public ExpandUrlValidator()
+    {
+        RuleFor(request => request.ShortCode)
+            .NotEmpty()
+            .Length(Link.ShortCodeMinLength, Link.ShortCodeMaxLength)
+            .Must(link => link.All(c => ShortCodeGeneratorService.AllowedChars.Contains(c)));
+    }
+}
