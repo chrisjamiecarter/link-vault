b9f8551a6ca0b96b0d1cd42d16b7868feffc1619
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-05-13 20:20:20 +0100

    Rewrite `GenerateQrCodeEndpoint`

--- src/LinkVault.Web.Api/Features/UrlShortening/GenerateQrCode/GenerateQrCodeEndpoint.cs
+++ src/LinkVault.Web.Api/Features/UrlShortening/GenerateQrCode/GenerateQrCodeEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace LinkVault.Web.Api.Features.UrlShortening.GenerateQrCode;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LinkVault.Web.Api.Features.UrlShortening.GenerateQrCode;
 
 public static class GenerateQrCodeEndpoint
 {
@@ -6,9 +8,28 @@ public static class GenerateQrCodeEndpoint
 
     public static void MapGenerateQrCodeEndpoint(this RouteGroupBuilder group)
     {
-        group.MapGet("/{shortCode}/qrcode", GenerateQrCodeHandler.Handle)
+        group.MapGet("/{shortCode}/qrcode", Handler)
             .AllowAnonymous()
             .WithName(Name)
-            .CacheOutput();
+            .CacheOutput()
+            .Produces<GenerateQrCodeResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .AddEndpointFilter<FluentValidationFilter<GenerateQrCodeRequest>>();
+    }
+
+    public static async Task<Results<Ok<GenerateQrCodeResponse>, NotFound, ProblemHttpResult>> Handler(
+        [AsParameters] GenerateQrCodeRequest request,
+        GenerateQrCodeHandler handler,
+        CancellationToken ct)
+    {
+        return await handler.HandleAsync(request, ct) switch
+        {
+            HandleResult<GenerateQrCodeResponse>.Success success => TypedResults.Ok(success.Value),
+            HandleResult<GenerateQrCodeResponse>.NotFound notFound => TypedResults.Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Link Not Found",
+                detail: notFound.Detail),
+            _ => TypedResults.Problem()
+        };
     }
 }
