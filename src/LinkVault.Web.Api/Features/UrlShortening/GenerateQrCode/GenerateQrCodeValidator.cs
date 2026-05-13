e152e5fe099d691ad80709566d03d89be294771d
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-05-13 20:19:19 +0100

    Add `GenerateQrCodeValidator`

---
+++ src/LinkVault.Web.Api/Features/UrlShortening/GenerateQrCode/GenerateQrCodeValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using LinkVault.Core.Entities;
+using LinkVault.Core.Services;
+
+namespace LinkVault.Web.Api.Features.UrlShortening.GenerateQrCode;
+
+public sealed class GenerateQrCodeValidator : AbstractValidator<GenerateQrCodeRequest>
+{
+    public GenerateQrCodeValidator()
+    {
+        RuleFor(request => request.ShortCode)
+            .NotEmpty()
+            .Length(Link.ShortCodeMinLength, Link.ShortCodeMaxLength)
+            .Must(link => link.All(c => ShortCodeGeneratorService.AllowedChars.Contains(c)));
+
+        When(request => request.BaseUrl is not null, () =>
+        {
+            RuleFor(request => request.BaseUrl!)
+                .Must(BeAValidAbsoluteHttpUri)
+                .WithMessage("Base URL must be a valid absolute HTTP or HTTPS URL.");
+        });
+    }
+
+    private static bool BeAValidAbsoluteHttpUri(string url)
+        => Uri.TryCreate(url, UriKind.Absolute, out var uri)
+           && uri.Scheme is "http" or "https";
+}
