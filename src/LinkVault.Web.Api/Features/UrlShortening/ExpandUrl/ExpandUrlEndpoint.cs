024490d4cc655eedef8799dd79c22e29949d6895
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-05-13 17:34:05 +0100

    Rewrite `ExpandUrlEndpoint`

--- src/LinkVault.Web.Api/Features/UrlShortening/ExpandUrl/ExpandUrlEndpoint.cs
+++ src/LinkVault.Web.Api/Features/UrlShortening/ExpandUrl/ExpandUrlEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace LinkVault.Web.Api.Features.UrlShortening.ExpandUrl;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LinkVault.Web.Api.Features.UrlShortening.ExpandUrl;
 
 public static class ExpandUrlEndpoint
 {
@@ -6,8 +8,27 @@ public static class ExpandUrlEndpoint
 
     public static void MapExpandUrlEndpoint(this RouteGroupBuilder group)
     {
-        group.MapGet("/{shortCode}", ExpandUrlHandler.Handle)
-                .AllowAnonymous()
-                .WithName(Name);
+        group.MapGet("/{shortCode}", Handler)
+            .AllowAnonymous()
+            .WithName(Name)
+            .Produces<ExpandUrlResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .AddEndpointFilter<FluentValidationFilter<ExpandUrlRequest>>();
+    }
+
+    public static async Task<Results<Ok<ExpandUrlResponse>, NotFound, ProblemHttpResult>> Handler(
+        [AsParameters] ExpandUrlRequest request,
+        ExpandUrlHandler handler,
+        CancellationToken ct)
+    {
+        return await handler.HandleAsync(request, ct) switch
+        {
+            HandleResult<ExpandUrlResponse>.Success success => TypedResults.Ok(success.Value),
+            HandleResult<ExpandUrlResponse>.NotFound notFound => TypedResults.Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Link Not Found",
+                detail: notFound.Detail),
+            _ => TypedResults.Problem()
+        };
     }
 }
