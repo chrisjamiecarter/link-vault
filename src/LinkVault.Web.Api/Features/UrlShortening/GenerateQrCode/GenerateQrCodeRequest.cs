e55028b571b742fa927b9ac0de93c5f58e4fe67a
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2026-05-13 20:10:10 +0100

    Add `GenerateQrCodeRequest` DTO

---
+++ src/LinkVault.Web.Api/Features/UrlShortening/GenerateQrCode/GenerateQrCodeRequest.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkVault.Web.Api.Features.UrlShortening.GenerateQrCode;
+
+public sealed record GenerateQrCodeRequest(
+    string ShortCode,
+    [FromQuery(Name = "baseUrl")] string? BaseUrl);
